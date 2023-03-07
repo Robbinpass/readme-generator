840e668389470038056e117a293bb53e2ca1d897
Robbinpass <robbinsandrew@outlook.com>
2023-03-07 12:39:52 -0500

    Added user input questions.

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,44 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'Title',
+        message: 'what is the title of our project?',
+    },
+
+     {
+        type: 'input',
+        name: 'Description',
+        message: 'Provide a brief description explaining what your project is and how it works.',
+     },
+
+     {
+        type: 'input',
+        name: "Installation",
+        message: 'What are the steps required to install your project?',
+
+     },
+
+     {
+        type: 'input',
+        name: 'Usage',
+        message: 'What is the intended usage for this prooject?',
+     },
+
+     {
+        type: 'input',
+        name: 'Credits',
+        message: 'List all collaborators, as well as any third-party assets that require attribution.',
+     },
+
+     
+
+];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
